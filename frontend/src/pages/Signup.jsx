bc418e1e3d52b58fbbd67bf98fa75547e9d9257f
NicolasSG <neco.beatles@gmail.com>
2026-07-23 22:12:52 -0300

    feat: add signup html

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,3 +1,60 @@
 export default function Signup() {
-  return <div>Signup</div>;
+  return (
+    <>
+      <section className="signup__welcome">
+        <div className="signup__welcome_logo">
+          <img src="" alt="" className="signup__welcome_logo-img" />
+          <h1 className="signup__welcome_logo-title">Talent Passport</h1>
+        </div>
+        <div className="signup__welcome_intro">
+          <h2 className="signup__welcome_intro_subtitle">
+            Quão pronto você está pro seu emprego dos sonhos?
+          </h2>
+          <p className="signup__welcome_intro_text">
+            Junte-se e transforme o sonho em realidade.
+          </p>
+        </div>
+        <div className="signup__welcome_instructions">
+          <div className="signup__welcome_instructions_list-numbers">
+            <p className="signup__welcome_instructions_list-number">1</p>
+            <p className="signup__welcome_instructions_list-number">2</p>
+            <p className="signup__welcome_instructions_list-number">3</p>
+          </div>
+          <div className="signup__welcome_instructions_list-texts">
+            <p className="signup__welcome_instructions_list-text">
+              Envie seu perfil
+            </p>
+            <p className="signup__welcome_instructions_list-text">
+              Faça a simulação
+            </p>
+            <p className="signup__welcome_instructions_list-text">
+              Estude com trilha personalizada
+            </p>
+          </div>
+        </div>
+      </section>
+      <section className="signup__form">
+        <p className="signup__title"></p>
+        <form action="" className="signup__form">
+          <input
+            type="email"
+            className="signup__form_email"
+            placeholder="Email"
+          />
+          <input
+            type="password"
+            className="signup__form_password"
+            placeholder="Email"
+          />
+          <button className="signup__form_button">Criar conta</button>
+          <div>
+            <p className="signup__account">Já tem conta?</p>
+            <a href="" className="signup__account_link">
+              Entrar
+            </a>
+          </div>
+        </form>
+      </section>
+    </>
+  );
 }
